b359f917295fdb8714f5b123f0ae6429a712362c
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-05-10 11:39:18 -0300

    layout base

---
+++ app/javascript/packs/app.vue
@@ -0,0 +1,25 @@
+<template>
+ <v-app>
+  <h4 class="white--text">Header Component</h4>
+  <v-content>
+   <h4 class="white--text">Main Component</h4>
+  </v-content>
+  <h4 class="white--text">Footer Component</h4>
+ </v-app>
+</template>
+ 
+<script>
+ export default {
+  data () {
+   return {
+ 
+   }
+  }
+ }
+</script>
+ 
+<style>
+ #app {
+  background-color: #0c0c0c !important;
+ }
+</style>
